52b36ed620622a8409c3a42d56e7d7603cffefe6
ZulekhaKorejo <kazuno373@gmail.com>
2026-07-03 16:40:01 +0500

    solved day 1 questions

--- 30_days_of_Python/Day_01/zulekha_sol_q1.py
+++ 30_days_of_Python/Day_01/zulekha_sol_q1.py
@@ -1,3 +1,19 @@
-Q1. Write a Python program to swap two variables
-Q2. Take user input and display it back to the user. 
-Q3. Write a program to check if a number is even or odd.
+# Q1. Write a Python program to swap two variables
+
+a , b = 5 , 10
+print(f"before swapping: {a , b}")
+a , b = b , a
+print(f"after swapping: {a , b}")
+
+# Q2. Take user input and display it back to the user. 
+
+#name = input("what's your name?")
+#print(f"Hi, nice to meet you {name}")
+
+#  Q3. Write a program to check if a number is even or odd.
+
+# num = int(input("tell me any number and i'll tell you back if it's odd or even: "))
+# if num %2 == 0:
+#     print(f'{num} is even')
+# else:
+#  print(f'{num} is odd')
