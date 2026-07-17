3fd18e0a74ed9cdcd964c2361db24a456b9f869c
Muhammad Younus Raza <m.younusraza.2005@gmail.com>
2026-07-17 14:19:44 +0500

    Day 01 solved

---
+++ 30_days_of_Python/Day_01/younus.py
@@ -0,0 +1,8 @@
+# Q1. Write a Python program to swap two variables. 
+
+a =1
+b=2
+
+print("a={a}")
+# Q2. Take user input and display it back to the user. 
+# Q3. Write a program to check if a number is even or odd.
